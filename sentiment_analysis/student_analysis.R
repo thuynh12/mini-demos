cae3e4de7f84263bd511553680836edddebaa958
Tracy Huynh <thuynh12@uw.edu>
2018-05-24 22:37:00 -0700

    Completed exercises

--- sentiment_analysis/student_analysis.R
+++ sentiment_analysis/student_analysis.R
@@ -7,8 +7,12 @@ library(syuzhet)
 # Add some happy ones, angry ones - you name it!
 student_sentences <- c('I really like the pie you gave me this morning.', 
                        'Your shoes suck and are just plain ugly.',
-                       'I\'d really truly love going out in this weather!'
+                       'I\'d really truly love going out in this weather!',
+                       'I really hate hospitals',
+                       'UW is good'
                        )
 
 # Analyze sentiment for student sentences
+sentiment <- data.frame(get_sentiment(student_sentences, method = 'syuzhet'))
 
+student.analysis <- cbind(sentence = student_sentences, sentiment)
--- webscraper-demo.R
+++ webscraper-demo.R
@@ -27,57 +27,85 @@ rank_data<-as.numeric(rank_data)
 head(rank_data)
 
 #Using CSS selectors to scrape the title section
+title.data.html <- html_nodes(webpage, '.lister-item-header a')
 
 #html to text 
+title.data <- html_text(title.data.html)
 
 #look at data
+head(title.data)
 
 #Using CSS selectors to scrape the description section
+description.data.html <- html_nodes(webpage,'.ratings-bar+ .text-muted')
 
 #Converting the description data to text
+description.data <- html_text(description.data.html)
 
 #look at data
+head(description_data)
 
 #Data-Preprocessing: removing '\n'
+description_data<-gsub("\n","",description_data)
 
 #Using CSS selectors to scrap the Movie runtime section
+runtime_data_html <- html_nodes(webpage,'.text-muted .runtime')
 
 #Converting the movie runtime data to text
+runtime_data <- html_text(runtime_data_html)
 
 #Let's have a look at the movie runtime
+head(runtime_data)
+
 
 #Data-Preprocessing: removing mins and converting it to numerical
-
+runtime_data<-gsub(" min","",runtime_data)
+runtime_data<-as.numeric(runtime_data)
 
 #Let's have another look at the runtime data
-
+head(runtime_data)
 
 #Converting the genre data to text
 
+genre.data.html <- html_nodes(webpage, '.genre')
+genre.data <- html_text(genre.data.html)  
 #Let's have a look at the genre
+head(genre.data)
 
 #Data-Preprocessing: removing \n
+genre.data <- gsub("\n", "", genre.data)
 
 #Data-Preprocessing: removing excess spaces
+genre.data <- gsub(" ", "", genre.data)
 
 #taking only the first genre of each movie
+genre.data <- gsub(",.*","",genre.data)
 
 #Convering each genre from text to factor
+genre.data <- as.factor(genre.data)
 
 #Let's have another look at the genre data
+head(genre.data)
 
 #Using CSS selectors to scrap the IMDB rating section
+rating_data_html <- html_nodes(webpage,'.ratings-imdb-rating strong')
 
 #Converting the ratings data to text
+rating_data <- html_text(rating_data_html)
 
 #Let's have a look at the ratings
+head(rating_data)
 
 #Data-Preprocessing: converting ratings to numerical
+rating_data <- as.numeric(rating_data)
 
 #Let's have another look at the ratings data
+head(rating_data)
+
 
 
 #Using CSS selectors to scrap the directors section
+directors_data_html <- html_nodes(webpage,'.text-muted+ p a:nth-child(1)')
+
 
 #Converting the directors data to text
 
